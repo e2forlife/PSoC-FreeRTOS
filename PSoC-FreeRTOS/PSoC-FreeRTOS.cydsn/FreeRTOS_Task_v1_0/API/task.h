8f4fd9bdf6e5d8617a8f14ae1642fe6021296a9f
e2forlife <chuck@e2forlife.com>
2016-01-13 15:25:07 -0500

    Added user code regions to tasks
    Added a merge region in to the task component header to allow for
    further customization of the generic task component.  this provides the
    ability to add user code that is externally referenced from other
    portions of the application, and generally makes the task component more
    useful.

--- PSoC-FreeRTOS/PSoC-FreeRTOS.cydsn/FreeRTOS_Task_v1_0/API/task.h
+++ PSoC-FreeRTOS/PSoC-FreeRTOS.cydsn/FreeRTOS_Task_v1_0/API/task.h
@@ -34,8 +34,29 @@
  */
 #if !defined(`$INSTANCE_NAME`_TASK_H)
 	#define `$INSTANCE_NAME`_TASK_H
+
+/*
+ * Add user task definitions, types, includes and other things in the below
+ * merge region to customize the task.
+ */
+/* `#START USER_TYPES_AND_DEFINES` */
+
+/* ---> ADD CODE HERE <--- */
+
+/* `#END` */
 	
 void `$INSTANCE_NAME`_Start( void );
 
+/*
+ * Add user function prototypes in the below merge region to add user
+ * functionality to the task definition.
+ */
+/* `#START USER_TASK_PROTOS` */
+
+/* ---> ADD CODE HERE <--- */
+
+/* `#END` */
+
+/* ------------------------------------------------------------------------ */
 #endif
 /* [] END OF FILE */
